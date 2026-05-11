3f13a2e6f9b0e6e8395faa859423f0e9976c05c0
LucasFernandes467 <lucas.f071@aluno.ifsc.edu.br>
2026-05-11 17:06:03 -0300

    commit 11/05

---
+++ php/blog/admin/index.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once './database/db.class.php';
+
+//instanciar um objeto da classe DB 
+$conn = new db("aluno");
+
+$dados = [
+    'nome' => "Lucas Fernandes 2",
+    'telefone' => "49985040369",
+    'email' => "caroline.m2007@aluno.ifsc.edu.br",
+];
+
+$conn->store($dados);
+echo "Inserido com sucesso!";
